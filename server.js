a954494a1e5ddf772f8a79a110347dd3820b60ce
Vincent Toon <vincenttoon22@gmail.com>
2022-09-25 23:21:35 -0500

    db queries for candidates inserted

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,26 +10,51 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // Your MySQL username,
-        user: 'root',
-        // Your MySQL password
-        password: 'rootintootin205',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "rootintootin205",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// // get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+//   // console.log(rows);
+// });
+
+// // Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
