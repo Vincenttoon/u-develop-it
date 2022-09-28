7da0066bf2ed6a60b85859001355e457ce008499
Vincent Toon <vincenttoon22@gmail.com>
2022-09-27 23:40:19 -0500

    voters added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: "root",
     // Your MySQL password
-    password: "rootintootin205",
+    password: "testy",
     database: "election",
   },
   console.log("Connected to the election database.")
